6024b823e5da187caebf9a9102ff7613b3fc700d
kangdabin <55787840+kangdabin@users.noreply.github.com>
2019-11-29 16:17:01 +0900

    Update ultra.py

--- ultra/ultra.py
+++ ultra/ultra.py
@@ -3,7 +3,7 @@ import time
 from time import sleep
 GPIO.setwarnings(False)
 
-class UltraSensor(self):
+class UltraSensor
     def __init__(self, trig, echo, buzzer):
         self.trig=trig
         self.echo=echo
@@ -15,19 +15,18 @@ class UltraSensor(self):
         GPIO.setup(buzzer, GPIO.OUT)
 
     def ultra(self):
-        try:
-            while True:
-                GPIO.output(trig, False)
+           while True:
+                GPIO.output(self.trig, False)
                 time.sleep(0.5)
         
-                GPIO.output(trig, True)
+                GPIO.output(self.trig, True)
                 time.sleep(0.00001)
-                GPIO.output(trig, False)
+                GPIO.output(self.trig, False)
         
-                while GPIO.input(echo)==0:
+                while GPIO.input(self.echo)==0:
                     pulse_start=time.time()
                 
-                while GPIO.input(echo)==1:
+                while GPIO.input(self.echo)==1:
                     pulse_end=time.time()
             
                 pulse_duration=pulse_end-pulse_start
@@ -35,16 +34,13 @@ class UltraSensor(self):
                 distance=round(distance,2)
         
                 if distance < 30 :
-                    GPIO.output(buzzer, GPIO.HIGH)
+                    GPIO.output(self.buzzer, GPIO.HIGH)
                     sleep(0.5)
                 else :
-                    GPIO.output(buzzer, GPIO.LOW)
+                    GPIO.output(self.buzzer, GPIO.LOW)
                     sleep(0.5)
             
                 print("거리 : ", distance, "cm")
-        
-        except Keyboardinterrupt:
-            GPIO.cleanup()
         
 x=UltraSensor(23,24,25)
 x.ultra()
