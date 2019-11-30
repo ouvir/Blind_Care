aaaab3827ea4ec768b258388f05715b153962c6f
ouvir <kilhyunii@naver.com>
2019-11-30 22:38:20 +0900

    FIX

--- main.py
+++ main.py
@@ -3,28 +3,29 @@ import time
 from mfrc522 import SimpleMFRC522
 import threading
 import turtle as t
+DISTANCE=0
 
 
 def ColorSensor(LED,S2,S3,signal):
+    global DISTANCE
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(LED, GPIO.OUT, initial=GPIO.HIGH)
     GPIO.setup(S2, GPIO.OUT, initial=GPIO.LOW)
     GPIO.setup(S3, GPIO.OUT, initial=GPIO.LOW)
     GPIO.setup(signal, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     NUM_CYCLES = 10
-
+    redcount=0
+    greencount=0
+    bluecount=0
+    whitecount=0
+    blackcount=0
+    JudgeConstant = 30
     while(1):
-
-        '''
         
-        '''
         #red filter set
         GPIO.output(S2, GPIO.LOW)
         GPIO.output(S3, GPIO.LOW)
-        count = 0
-        JudgeConstant = 30
 
-        count = count + 1
         time.sleep(0.1) #delay 0.1sec
         start = time.time()
         for impulse_count in range(NUM_CYCLES):
@@ -59,7 +60,7 @@ def ColorSensor(LED,S2,S3,signal):
             green = 0
 
         #calibration
-        white_level= 14000
+        white_level= 18000
         black_level= 5000
         raw_rgb = [red, green, blue]
         rgb = [0,0,0]
@@ -73,8 +74,8 @@ def ColorSensor(LED,S2,S3,signal):
                 rgb[i] = 255
             elif rgb[i] < 0:
                 rgb[i] = 0
-        # print("red:", rgb[0], "blue:",rgb[1], "green:",rgb[2])
-         # print("rawRGB:", raw_rgb)
+        print("red:", rgb[0], "green:",rgb[1], "blue:",rgb[2])
+        print("rawRGB:", raw_rgb)
 
         offsetRB = rgb[0] - rgb[2]
         offsetRG = rgb[0] - rgb[1]
@@ -86,29 +87,53 @@ def ColorSensor(LED,S2,S3,signal):
         average=int((rgb[0]+rgb[1]+rgb[2])/3)
         if average < 30:
             blackcount = blackcount + 1
+            print("black!")
+            #buzzer!!
         elif average > 230:
             whitecount = whitecount + 1
         elif rgb[0] > rgb[1] and rgb[0] > rgb[2]:
+            redcount += 1
             if offsetRG > JudgeConstant and offsetRB > JudgeConstant:
                 print("this is red")
         elif rgb[1] > rgb[0] and rgb[1] > rgb[2]:
-            if offsetBG > JudgeConstant and offsetBR > JudgeConstant:
-                print("this is blue")
-        elif rgb[2] > rgb[0] and rgb[2] > rgb[1]:
-            if offsetGR > JudgeConstant and offsetGB > JudgeConstant
+            greencount+=1
+            if offsetGB > JudgeConstant and offsetGR > JudgeConstant:
                 print("this is green")
+        elif rgb[2] > rgb[0] and rgb[2] > rgb[1]:
+            bluecount+=1
+            if offsetBR > JudgeConstant and offsetBG > JudgeConstant:
+                print("this is blue")
         else:
             print("Undefined")
 
 
-        #drawing color
+        if greencount > bluecount +5 and greencount > redcount +5:
+            global DISTANCE
+            print("green dominant")
+            redcount = 0
+            bluecount = 0
+            greencount = 0
+            balckcount = 0
+            whitecount = 0
+            DISTANCE = 1
 
-        #t.colormode(255)
-        #t.pen(pensize="20")
-        #t.pencolor(rgb[0],rgb[1],rgb[2])
-        #t.goto(0,0)
-        #t.forward(40)
+        elif bluecount > greencount + 5 and bluecount > redcount + 5:
+            print("blue dominant")
+            redcount = 0
+            bluecount = 0
+            greencount = 0
+            blackcount = 0
+            whitecount = 0
+            DISTANCE = 2
 
+        elif redcount > bluecount + 5 and redcount > greencount + 5:
+            print("red dominant")
+            redcount = 0
+            bluecount = 0
+            greencount = 0
+            blackcount = 0
+            whitecount = 0
+            DISTANCE= 3
 
 
 
